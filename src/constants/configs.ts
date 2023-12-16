088252eee51f5f1be29e321a6aee94f33c918668
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2023-12-16 23:13:46 +0600

    Configs

---
+++ src/constants/configs.ts
@@ -0,0 +1,25 @@
+const isProduction = process.env.NODE_ENV === "production";
+
+export const configuration = {
+  site: {
+    name: "Octolane AI",
+    description: "LLM Powered B2B Data Enrichment API",
+    themeColor: "#ffffff",
+    themeColorDark: "#0a0a0a",
+    siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+    siteName: "Octolane AI",
+    twitterHandle: "octolane_app",
+    githubHandle: "octolane-org",
+    convertKitFormId: "",
+    locale: process.env.NEXT_PUBLIC_DEFAULT_LOCALE,
+  },
+  isProduction,
+  environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
+  sentry: {
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  },
+  posthog: {
+    apiKey: process.env.NEXT_PUBLIC_POSTHOG_API_KEY,
+    apiHost: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
+  },
+};
