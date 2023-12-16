586bff3f72a7d55a656cdd66b232ad07eadd46ad
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2023-12-17 00:55:29 +0600

    Basic google auth donec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { GoogleLoginButton } from "@/components/GoogleLoginButton";
 import { Button } from "@/components/ui/button";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  const session = useSession();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Google</Button>
+      {session.status === "authenticated" ? (
+        <Button onClick={() => signOut()}>
+          Logout as {session.data.user?.name}
+        </Button>
+      ) : (
+        <GoogleLoginButton />
+      )}
     </main>
   );
 }
