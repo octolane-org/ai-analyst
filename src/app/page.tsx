b5da845b9e853ebd0222f23f78546c01e28ea9bd
One Chowdhury <coffeeandone@gmail.com>
2023-12-17 03:56:10 -0500

    action card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,68 @@
 "use client";
 
-import { GoogleLoginButton } from "@/components/GoogleLoginButton";
+import { ActionCard } from "@/components/ActionCard";
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 export default function Home() {
   const session = useSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {session.status === "authenticated" ? (
-        <Button onClick={() => signOut()}>
-          Logout as {session.data.user?.name}
-        </Button>
-      ) : (
-        <GoogleLoginButton />
-      )}
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-between space-y-16 ">
+      <Container>
+        <div
+          className={
+            "my-12 flex flex-col items-center md:flex-row lg:my-16" +
+            " mx-auto flex-1 justify-center  " +
+            " duration-1000 slide-in-from-top-12" +
+            "relative before:absolute before:inset-0 before:h-80 before:pointer-events-none before:bg-gradient-to-b before:from-zinc-100 before:-z-10"
+          }
+        >
+          <div className="text-center ">
+            <h1 className="font-inter-tight text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-zinc-500 via-zinc-900 to-zinc-900 pb-4">
+              1-Click B2B Data Enrichment
+            </h1>
+            <p className="text-lg text-zinc-500 mb-8">
+              Simply upload your file, and with one click and get the enriched
+              data you need to drive your business forward.
+            </p>
+            <div className="flex space-x-5 pb-5">
+              <ActionCard
+                cardTitle="Enrich People"
+                cardDescription="Upload a CSV or Excel of up to 500 people email and enrich their information for free."
+                buttonText="Upload CSV"
+              />
+
+              <ActionCard
+                cardTitle="Enrich Company"
+                cardDescription="Upload a CSV or Excel of up to 500 company website and enrich information for free.."
+                buttonText="Upload CSV"
+              />
+            </div>
+            <Button variant="cta">
+              <Link href="/dashboard">Go to Dashboard</Link>
+              <svg className="w-4 h-5 ml-2"></svg>
+            </Button>
+
+            {/* Trusted by section */}
+            <div>
+              <span className={"text-xs text-gray-500 dark:text-gray-400"}>
+                No credit card required.
+              </span>
+            </div>
+          </div>
+
+          {/* {session.status === "authenticated" ? (
+            <Button onClick={() => signOut()}>
+              Logout as {session.data.user?.name}
+            </Button>
+          ) : (
+            <GoogleLoginButton />
+          )} */}
+        </div>
+      </Container>
+    </div>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-sm text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -18,11 +18,12 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        cta: "bg-zinc-900 text-zinc-100 hover:bg-zinc-800 hover:text-white w-full shadow",
       },
       size: {
         default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
+        sm: "h-8 rounded-sm px-3 text-xs",
+        lg: "h-10 rounded-sm px-8",
         icon: "h-9 w-9",
       },
     },
@@ -42,7 +43,15 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    return (
+    return variant === "cta" ? (
+      <div className="w-full text-white rounded-md p-1 flex items-center justify-center sm:bg-slate-200">
+        <Comp
+          className={cn(buttonVariants({ variant, size, className }))}
+          ref={ref}
+          {...props}
+        />
+      </div>
+    ) : (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
