e57b5ac139c09116e4876294870b77877e290365
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2023-12-20 00:22:20 +0600

    Work email restriction

--- src/types/common.type.ts
+++ src/types/common.type.ts
@@ -1,3 +1,8 @@
 export type ObjectString = Record<string, string>;
 
 export type ValueOf<T> = T[keyof T];
+
+export type ServerPageProps<Params = any, SearchParams = any> = {
+  params: Params;
+  searchParams: SearchParams;
+};
